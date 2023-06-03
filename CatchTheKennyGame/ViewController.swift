310935524364558b509415eb75da697dbc95f12e
seydaydiin <seydaydiin0@gmail.com>
2023-06-03 20:49:10 +0300

    First Commit

--- CatchTheKennyGame/ViewController.swift
+++ CatchTheKennyGame/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var nameText = ""
+    var surnameText = ""
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
+    @IBOutlet weak var surnameTextField: UITextField!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        nameText = nameTextField.text!
+        surnameText = surnameTextField.text!
+        
+        if segue.identifier == "toSecondVC" {
+            let destinationVC = segue.destination as! SecondViewController
+            destinationVC.username = nameText
+            destinationVC.userSurname = surnameText
+        }
+    }
+    
 }
 
